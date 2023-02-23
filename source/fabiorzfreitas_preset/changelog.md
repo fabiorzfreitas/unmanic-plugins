bc6c69967083dfbf3539732d8f371cacd0e9b98a
fabiorzfreitas <mckfabio@gmail.com>
2023-02-23 15:56:33 -0300

    First commit

---
+++ source/fabiorzfreitas_preset/changelog.md
@@ -0,0 +1,23 @@
+**<span style="color:#56adda">1.1.0</span>**
+- Small fix on logger output.
+
+**<span style="color:#56adda">1.1.0</span>**
+- New logger output, comments on code. Could use type annotation and perhaps another post-processing runner.
+
+**<span style="color:#56adda">1.0.0</span>**
+- Everything seems to be working. Further development will simply polish the code.
+
+**<span style="color:#56adda">0.5.0</span>**
+- Implemented MKV remuxing.
+- 
+**<span style="color:#56adda">0.4.0</span>**
+- Almost final, needs to implement cache settings and MKV remuxing to otherwise good files. This commit also predates the pull request to lib.ffmpeg.probe.py to add support for chapters.
+
+**<span style="color:#56adda">0.3.0</span>**
+- Almost fully functional, it still needs to parse chapters, attachments and metadata. The rest works.
+
+**<span style="color:#56adda">0.2.0</span>**
+- Basic functionality seems ok.
+
+**<span style="color:#56adda">0.1.0</span>**
+- Not working yet.
---
+++ source/fabiorzfreitas_preset/plugin.py
@@ -0,0 +1,431 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    fabiorzfreitas_preset.plugin.py
+ 
+    Written by:               Fabiorzfreitas <mckfabio@gmail.com>
+    Date:                     Sunday, February 2nd, 2023, 13:00
+ 
+    Copyright:
+           Copyright (C) Josh Sunnex - All Rights Reserved
+ 
+           Permission is hereby granted, free of charge, to any person obtaining a copy
+           of this software and associated documentation files (the "Software"), to deal
+           in the Software without restriction, including without limitation the rights
+           to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+           copies of the Software, and to permit persons to whom the Software is
+           furnished to do so, subject to the following conditions:
+  
+           The above copyright notice and this permission notice shall be included in all
+           copies or substantial portions of the Software.
+  
+           THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+           EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+           MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+           IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+           DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+           OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+           OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+import logging
+
+import os
+from fabiorzfreitas_preset.lib.ffmpeg import Probe, Parser
+
+# Configures plugin logger
+logger = logging.getLogger("Unmanic.Plugin.fabiorzfreitas_preset")
+
+# Formats logger output
+def logger_output(line: str):
+    """
+    Simple function to make the logger output more readable
+    """
+
+    line_len:str  = '-' * len(line)
+
+    logger.debug(line_len)
+    logger.debug(line)
+    logger.debug(line_len)
+
+    return
+
+
+def on_library_management_file_test(data: dict) -> None:
+    """
+    Runner function - enables additional actions during the library management file tests.
+    The 'data' object argument includes:
+        library_id                      - The library that the current task is associated with
+        path                            - String containing the full path to the file being tested.
+        issues                          - List of currently found issues for not processing the file.
+        add_file_to_pending_tasks       - Boolean, is the file currently marked to be added to the queue for processing.
+        priority_score                  - Integer, an additional score that can be added to set the position of the new task in the task queue.
+        shared_info                     - Dictionary, information provided by previous plugin runners. This can be appended to for subsequent runners.
+    :param data:
+    :return:
+    """
+    
+    # Sets working path
+    abspath: str = data['path']
+
+    testing_line: str = f'[TESTING] Testing file {abspath}'
+    logger_output(testing_line)
+
+    # Skips cache files
+    if os.path.basename(abspath).split('.')[-2] == 'cache':
+        
+        cache_line: str = f'[TESTING] File {abspath} is cache, skipping'
+        logger_output(cache_line)
+        
+        data['add_file_to_pending_tasks'] = False
+
+        return
+
+    # Skips .part files
+    if os.path.splitext(abspath)[1] == '.part':
+        
+        part_line: str = f'[TESTING] File {abspath} extension is .part, skipping'
+        logger_output(part_line)
+        
+        data['add_file_to_pending_tasks'] = False
+
+        return
+
+    # Gets file probe
+    probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
+    if not probe:
+        # File not able to be probed by ffprobe
+        return
+
+    ffprobe_data: dict = probe.get_probe()
+
+    # Defines paths and shared_info
+    shared_info: dict = {}
+    source_dirpath: str = f"{os.path.split(data['path'])[0]}"
+    source_dirpath_replaced: str = source_dirpath.replace('\\', '/')
+    show_dir: str = source_dirpath_replaced.split('/')[-2]
+    basename: str= f"{os.path.split(data['path'])[1]}"
+
+    # Tests if file is already in Optimized folder
+    if show_dir == 'Optimized for TV' or os.path.exists(f'{source_dirpath_replaced}/Plex Versions/Optimized for TV/{show_dir}/{basename}'):
+
+        optimized_line: str = f'[TESTING] File {abspath} already has been optimized, skipping'
+        logger_output(optimized_line)
+        
+        data['add_file_to_pending_tasks'] = False
+
+        return
+    
+    # Tests if container is .mkv
+    # A second pass will be needed if this matches
+    if os.path.splitext(abspath)[1] != '.mkv':
+
+        testing_mkv_line: str = f'[TESTING] File {abspath} container is not .mkv, adding to queue'
+        logger_output(testing_mkv_line)
+
+        data['add_file_to_pending_tasks'] = True
+        shared_info['container_is_not_mkv'] = True
+
+        return
+
+    # Checks the first video stream for x264
+    for stream in ffprobe_data['streams']:
+        
+        if stream['codec_type'] == 'video' and stream['codec_name'] != 'h264':
+
+            testing_x264_line: str = f'[TESTING] File {abspath} video stream is not x264, adding to queue'
+            logger_output(testing_x264_line)
+
+            shared_info['non_h264'] = True
+            # This function doesn't return yet, as the file still needs to be checked for audio
+
+    # Checks if the video stream is the first stream
+    if ffprobe_data['streams'][0]['codec_type'] != 'video':
+        
+        testing_video_0_line: str = f'[TESTING] File {abspath} does not have video as the first stream, adding to queue'
+        logger_output(testing_video_0_line)
+        
+        data['add_file_to_pending_tasks'] = True
+        
+        for stream in data['streams']:
+            if stream['codec_type'] == 'video':
+                shared_info['non_0_video_stream'] = True
+                shared_info['video_stream_index'] = stream['index']
+                
+                break
+                # This function doesn't return yet, as the file still needs to be checked for audio
+
+    # Checks if first audio stream is ac3
+    if ffprobe_data['streams'][1]['codec_type'] == 'audio' and ffprobe_data['streams'][1]['codec_name'] != 'ac3':
+        
+        testing_ac3_line: str = f'[TESTING] File {abspath} does not have ac3 as the first audio stream, adding to queue'
+        logger_output(testing_ac3_line)
+
+        data['add_file_to_pending_tasks'] = True
+        shared_info['first_audio_is_not_ac3'] = True
+        
+        return
+    
+    # Checks if there are chapters
+    if ffprobe_data['chapters'] != []:
+
+        testing_chapters_line: str = f'[TESTING] File {abspath} has chapters, processing'
+        logger_output(testing_chapters_line)
+
+        data['add_file_to_pending_tasks'] = True
+        shared_info['has_chapters'] = True
+
+        return
+
+    # Checks streams for subtitles, attachments and metadata
+    for stream in ffprobe_data['streams']:
+       
+        if stream['codec_type'] == 'subtitle':
+            
+            testing_subtitles_line: str = f'[TESTING] File {abspath} has subtitles, adding to queue'
+            logger_output(testing_subtitles_line)
+
+            data['add_file_to_pending_tasks'] = True
+            shared_info['has_subtitles'] = True
+            
+            return
+       
+        if stream['codec_type'] != 'audio' and stream['codec_type'] != 'video':
+
+            testing_attachment_line: str = f'[TESTING] File {abspath} has non-audio, non-subtitle stream, likely an attachment, adding to queue'
+            logger_output(testing_attachment_line)
+
+            data['add_file_to_pending_tasks'] = True
+            shared_info['has_attachment'] = True
+
+            return
+
+        allowed_tags: list = ['language', 'DURATION', 'ENCODER']
+        if not set(stream['tags'].keys()).issubset(allowed_tags):
+            
+            testing_tags_line: str = f'[TESTING] File {abspath} has unwanted metadata, adding to queue'
+            logger_output(testing_tags_line)
+
+            data['add_file_to_pending_tasks'] = True
+            shared_info['has_unwanted_metadata'] = True
+
+            return
+
+    # If file passes all checks, it's skipped    
+    testing_skip_line: str = f"[TESTING] File {abspath} doesn't need processing, skipping"
+    logger_output(testing_skip_line)
+    
+    data['add_file_to_pending_tasks'] = False
+    return
+            
+
+def on_worker_process(data: dict) -> None:
+    """
+    Runner function - enables additional configured processing jobs during the worker stages of a task.
+
+    The 'data' object argument includes:
+        worker_log              - Array, the log line_lens that are being tailed by the frontend. Can be left empty.
+        library_id              - Number, the library that the current task is associated with.
+        exec_command            - Array, a subprocess command that Unmanic should execute. Can be empty.
+        command_progress_parser - Function, a function that Unmanic can use to parse the STDOUT of the command to collect progress stats. Can be empty.
+        file_in                 - String, the source file to be processed by the command.
+        file_out                - String, the destination that the command should output (may be the same as the file_in if necessary).
+        original_file_path      - String, the absolute path to the original file.
+        repeat                  - Boolean, should this runner be executed again once completed with the same variables.
+
+    :param data:
+    :return:
+    
+    """
+
+    # Get the path to the file
+    abspath: str = data['original_file_path']
+
+    processing_line: str = f'[PROCESSING] Processing file {abspath}'
+    logger_output(processing_line)
+
+    # Defines working variables
+    path, basename = os.path.split(abspath)
+    data['path'] = abspath
+    no_ext: str = os.path.splitext(basename)[0] 
+    file_in: str = data['file_in']
+    data['file_out'] = f'{path}/{no_ext}.cache.mkv'
+    file_out: str = data['file_out']
+    video_codec: str = '-c:v:0 copy'
+
+    # Get file probe
+    probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
+    if not probe:
+        # File not able to be probed by ffprobe
+        return
+    ffprobe_data: dict = probe.get_probe()
+
+    # Set the parser
+    parser = Parser(logger)
+    parser.set_probe(probe)
+    data['command_progress_parser'] = parser.parse_progress
+
+    # Tests if container is .mkv
+    # A second pass will be needed if this matches
+    if os.path.splitext(abspath)[1] != '.mkv':
+
+        processing_mkv_line: str = f'[PROCESSING] File {abspath} container is not .mkv, processing'
+        logger_output(processing_mkv_line)
+
+        data['exec_command'] = f'ffmpeg -i {file_in} -c copy {file_out}'
+
+        return
+        
+    # Checks the first video stream for x264
+    for stream in ffprobe_data['streams']:
+        
+        if stream['codec_type'] == 'video' and stream['codec_name'] != 'h264':
+
+            processing_x264_line: str = f'[PROCESSING] File {abspath} video stream is not x264, setting video codec'
+            logger_output(processing_x264_line)
+
+            video_codec: str = '-c:v:0 h264' # type: ignore
+            break
+            # This function doesn't return yet, as the file still needs to be checked for audio
+        
+    # Checks if the video stream is the first stream
+    if ffprobe_data['streams'][0]['codec_type'] != 'video':
+        
+        processing_video_0_line: str = f'[PROCESSING] File {abspath} does not have video as the first stream, processing'
+        logger_output(processing_video_0_line)
+        
+        for stream in data['streams']:
+            if stream['codec_type'] == 'video':
+                data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+    
+    # Checks if first audio stream is ac3
+    if ffprobe_data['streams'][1]['codec_type'] == 'audio' and ffprobe_data['streams'][1]['codec_name'] != 'ac3':
+        
+        processing_ac3_line: str = f'[PROCESSING] File {abspath} does not have ac3 as the first audio stream, processing'
+        logger_output(processing_ac3_line)
+
+        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a:0 -c:a:0 ac3 -map 0:a:0 -c:a:1 copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+        
+        return
+    
+    # If video check matches, processing starts after checking audio
+    if video_codec == '-c:v:0 x264':
+        
+        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+
+        return
+
+    # Checks if there are chapters
+    if ffprobe_data['chapters'] != []:
+
+        processing_chapters_line: str = f'[PROCESSING] File {abspath} has chapters, processing'
+        logger_output(processing_chapters_line)
+
+        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+
+        return
+    
+    # Checks streams for subtitles, attachments and metadata
+    for stream in ffprobe_data['streams']:
+       
+        if stream['codec_type'] == 'subtitle':
+            
+            processing_subtitles_line: str = f'[PROCESSING] File {abspath} has subtitles, processing'
+            logger_output(processing_subtitles_line)
+
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+            
+            return
+       
+        if stream['codec_type'] != 'audio' and stream['codec_type'] != 'video':
+
+            processing_attachment_line: str = f'[PROCESSING] File {abspath} has non-audio, non-subtitle stream, likely an attachment, processing'
+            logger_output(processing_attachment_line)
+
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+
+            return
+
+        allowed_tags = ['language', 'DURATION', 'ENCODER']
+        if not set(stream['tags'].keys()).issubset(allowed_tags):
+            
+            processing_tags_line: str = f'[PROCESSING] File {abspath} has unwanted metadata, processing'
+            logger_output(processing_tags_line)
+
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+            
+            return
+
+    return
+
+
+def on_postprocessor_file_movement(data: dict) -> None:
+    """
+    Runner function - configures additional postprocessor file movements during the postprocessor stage of a task.
+
+    The 'data' object argument includes:
+        source_data             - Dictionary containing data pertaining to the original source file.
+        remove_source_file      - Boolean, should Unmanic remove the original source file after all copy operations are complete.
+        copy_file               - Boolean, should Unmanic run a copy operation with the returned data variables.
+        file_in                 - The converted cache file to be copied by the postprocessor.
+        file_out                - The destination file that the file will be copied to.
+
+    :param data:
+    :return:
+    """
+
+    # Defines paths
+    data['path'] = data['source_data']['abspath']
+    abspath: str = data['path']
+    source_dirpath: str = f"{os.path.split(data['source_data']['abspath'])[0]}"
+    source_dirpath_replaced: str = source_dirpath.replace('\\', '/')
+    show_dir: str = source_dirpath_replaced.split('/')[-2]
+    basename: str = data['source_data']['basename']
+
+
+    post_processing_metadata_line: str = f'[POST-PROCESSING] Post-processing file {abspath}'
+    logger_output(post_processing_metadata_line)
+
+    # Get file probe
+    probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
+    if not probe:
+        # File not able to be probed by ffprobe
+        return
+
+    ffprobe_data: dict = probe.get_probe()
+   
+    # Sets function parameters
+    data['remove_source_file'] = False
+    data['copy_file'] = True
+    data['run_default_file_copy'] = False
+    data['file_out'] = f'{source_dirpath_replaced}/{basename}'
+
+    # Resets output location for files with a new container
+    if os.path.splitext(abspath)[1] != '.mkv':
+
+        post_processing_mkv_line: str = f'[POST-PROCESSING] File {abspath} container is now .mkv, moving'
+        logger_output(post_processing_mkv_line)
+
+        data['remove_source_file'] = True
+        data['run_default_file_copy'] = True
+
+        return
+
+    # Sets Plex Optimized Versions as output folder for x264 transcodes
+    for stream in ffprobe_data['streams']:
+        
+        if stream['codec_type'] == 'video' and stream['codec_name'] != 'h264':
+
+            post_processing_x264_line: str = f'[POST-PROCESSING] File {abspath} video stream is not x264, setting different output'
+            logger_output(post_processing_x264_line)
+
+            os.makedirs(f'{source_dirpath_replaced}/Plex Versions/Optimized for TV/{show_dir}', exist_ok= True)
+
+            data['file_out'] = f'{source_dirpath_replaced}/Plex Versions/Optimized for TV/{show_dir}/{basename}'
+            
+            break
+
+    return
+
